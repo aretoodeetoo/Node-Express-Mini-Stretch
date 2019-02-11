4a2b1490099cec9799f4c3f81e1cfcfbba47641b
croy <roy.christie95@gmail.com>
2019-02-11 17:57:18 -0500

    Added reducers for actions

---
+++ users/src/store/reducers/index.js
@@ -0,0 +1,37 @@
+import {
+    FETCH_USERS_SUCCESS,
+    ADD_USER_SUCCESS,
+    DELETE_USER_SUCCESS
+} from '../actions';
+
+const initialState = {
+    users: [],
+    isAddingUser: false
+}
+
+const users = (state = initialState, action) => {
+    switch (action.type){
+        case FETCH_USERS_SUCCESS:
+        return{
+            ...state,
+            users: action.payload
+        }
+        case ADD_USER_SUCCESS:
+        return{
+            ...state,
+            users: action.payload,
+            isAddingUser: false,
+            error: ''
+        }
+        case DELETE_USER_SUCCESS:
+        return{
+            ...state,
+            users: action.payload,
+            error: ''
+        }
+        default:
+        return state;
+    }
+}
+
+export default friends;
